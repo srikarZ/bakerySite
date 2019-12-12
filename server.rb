b1f7f1b29d36d964376db557e095345b8f79eb66
Srikar Reddy Ramesh Kumar <srameshkumar@nyc.yearup.org>
2019-12-12 16:46:37 -0500

    Added cards and navbar. Implemented grid on the cards.

--- server.rb
+++ server.rb
@@ -3,3 +3,19 @@ require 'sinatra'
 get '/' do
   erb :layout
 end
+
+get '/cookies.erb' do
+  erb :cookies
+end
+
+get '/cakes.erb' do
+  erb :cakes
+end
+
+get '/muffins.erb' do
+  erb :muffins
+end
+
+get '/layout.erb' do
+  erb :layout
+end
