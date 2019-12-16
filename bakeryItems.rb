155339e5b2b5591a9f0423c1cc3a929c569355cb
Srikar Reddy Ramesh Kumar <srameshkumar@nyc.yearup.org>
2019-12-16 15:34:15 -0500

    Used the instance variables properties to fill description and price

--- bakeryItems.rb
+++ bakeryItems.rb
@@ -1,5 +1,5 @@
 class Cake
-  attr_reader :description, :price
+  attr_accessor :description, :price
   def initialize(description, price)
     @description = description
     @price = price
@@ -22,23 +22,23 @@ class Muffin
   end
 end
 
-cheeseCake = Cake.new("Taste the one of the best homemade cheesecake.", "20$")
-lemonCake = Cake.new("A sweet and lemony treat baked in one of the best bakeries of NY.", "13$")
-carrotCake = Cake.new("Crunchy Carrots and a sweet savory cake nothing could go wrong.", "15$")
-redVelvet = Cake.new("A light and fluffy cake with the taste of perfection.", "25$")
-marbleCake = Cake.new("Chocolate swirls in between each layer when you cut the cake.", "11$")
-chocolateMousse = Cake.new("", "25$")
+@cheeseCake = Cake.new("Taste the one of the best homemade cheesecake.", "20$")
+@lemonCake = Cake.new("A sweet and lemony treat baked in one of the best bakeries of NY.", "13$")
+@carrotCake = Cake.new("Crunchy Carrots and a sweet savory cake nothing could go wrong.", "15$")
+@redVelvet = Cake.new("A light and fluffy cake with the taste of perfection.", "25$")
+@marbleCake = Cake.new("Chocolate swirls in between each layer when you cut the cake.", "11$")
+@chocolateMousse = Cake.new("Enjoy a mouth watering chocolatey surprise.", "25$")
 
-peanutButterCookie = Cookie.new("", "1$")
-chocolateChipCookie = Cookie.new("", "1$")
-butterCookie = Cookie.new("", "1$")
-coconutCookie = Cookie.new("", "1$")
-vanillaCookie = Cookie.new("", "1$")
-shortBreadCookie = Cookie.new("", "1$")
+@peanutButterCookie = Cookie.new("The classic peanut butter cookie.", "1$")
+@chocolateChipCookie = Cookie.new("Homestyle chocolate chip cookies.", "1$")
+@butterCookie = Cookie.new("A buttery cookie that melts in your mouth.", "1$")
+@coconutCookie = Cookie.new("A cookie with crunchy coconut flakes.", "1$")
+@vanillaCookie = Cookie.new("A hint of vanilla and a wafery texture", "1$")
+@shortBreadCookie = Cookie.new("Not sweet but buttery, ideal for a snack", "1$")
 
-yogurtMuffin = Muffin.new("", "2$")
-chocolateMuffin = Muffin.new("", "2$")
-cornMuffin = Muffin.new("", "1$")
-blueberryMuffin = Muffin.new("", "2$")
-cinnamonMuffin = Muffin.new("", "1.5$")
-mangoMuffin = Muffin.new("", "2$")
+@yogurtMuffin = Muffin.new("A light moist delicious muffin made with greek yogurt", "2$")
+@chocolateMuffin = Muffin.new("A chocolatey soft muffin.", "2$")
+@cornMuffin = Muffin.new("A muffin made from corn, fewer calories a great snack.", "1$")
+@blueberryMuffin = Muffin.new("An all time favorite, made with fresh blueberries.", "2$")
+@cinnamonMuffin = Muffin.new("Tender sweet and a cinnamon flavor.", "1.5$")
+@mangoMuffin = Muffin.new("Mango flavor in a muffin? Try this bad boy out", "2$")
--- server.rb
+++ server.rb
@@ -6,14 +6,32 @@ get '/' do
 end
 
 get '/cookies' do
+  @peanutButterCookie = Cookie.new("The classic peanut butter cookie.", "1$")
+  @chocolateChipCookie = Cookie.new("Homestyle chocolate chip cookies.", "1$")
+  @butterCookie = Cookie.new("A buttery cookie that melts in your mouth.", "1$")
+  @coconutCookie = Cookie.new("A cookie with crunchy coconut flakes.", "1$")
+  @vanillaCookie = Cookie.new("A hint of vanilla and a wafery texture", "1$")
+  @shortBreadCookie = Cookie.new("Not sweet but buttery, ideal for a snack", "1$")
   erb :cookies
 end
 
 get '/cakes' do
+  @cheeseCake = Cake.new("Taste the one of the best homemade cheesecake.", "20$")
+  @lemonCake = Cake.new("A sweet and lemony treat baked in one of the best bakeries of NY.", "13$")
+  @carrotCake = Cake.new("Crunchy Carrots and a sweet savory cake nothing could go wrong.", "15$")
+  @redVelvet = Cake.new("A light and fluffy cake with the taste of perfection.", "25$")
+  @marbleCake = Cake.new("Chocolate swirls in between each layer when you cut the cake.", "11$")
+  @chocolateMousse = Cake.new("Enjoy a mouth watering chocolatey surprise.", "25$")
   erb :cakes
 end
 
 get '/muffins' do
+  @yogurtMuffin = Muffin.new("A light moist delicious muffin made with greek yogurt", "2$")
+  @chocolateMuffin = Muffin.new("A chocolatey soft muffin.", "2$")
+  @cornMuffin = Muffin.new("A muffin made from corn, fewer calories a great snack.", "1$")
+  @blueberryMuffin = Muffin.new("An all time favorite, made with fresh blueberries.", "2$")
+  @cinnamonMuffin = Muffin.new("Tender sweet and a cinnamon flavor.", "1.5$")
+  @mangoMuffin = Muffin.new("Mango flavor in a muffin? Try this bad boy out", "2$")
   erb :muffins
 end
 
